fae2b8ebd1e3481b33a3081ab121659f9259aaf9
ptruffault <ptruffault@hotmail.com>
2021-04-06 10:12:12 +0200

    some small modif on class taquin

--- src/npuzzle.py
+++ src/npuzzle.py
@@ -6,14 +6,15 @@ from utils import *
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("file", help="Must be a valid file")
+	parser.add_argument("file", help="Must be a valid taquin file")
 	parser.add_argument("-v", "--view", action="store_true", default=False, help="enable visualizer")
+	parser.add_argument("-f", "--heuristic", default='manhatan', help="chose the heuristic function")
 	args = parser.parse_args()
 	if not getattr(args, 'file'):
 		error('file expected')
 	board = getBoard(args.file)
 	if board:
-		TAQUIN = Taquin(board)
+		TAQUIN = Taquin(board, args.heuristic)
 		solution = TAQUIN.resolv()
 		if args.view:
 			TaquinGame(board, solution)
--- src/taquin.py
+++ src/taquin.py
@@ -1,12 +1,14 @@
 from utils import *
 from math import *
 from copy import deepcopy
+import argparse
 
 class Taquin(object):
-	def __init__(self, board):
-		self.board = deepcopy(board)
+	def __init__(self, board, heuristic='manhatan'):
 		self.size = len(board)
-		if not self.isSolvable():
+		self.board = deepcopy(board)
+		self.heuristic = heuristic
+		if not self.isSolvable(board):
 			error('unsolvable')
 		if self.isSolved():
 			error('Already solved')
@@ -14,22 +16,25 @@ class Taquin(object):
 		self.timeComplexity = 1
 		self.spaceComplexity = 1
 		self.solution = []
+		self.bestScore = 99999
+		self.tmpSolution = []
 
 
 	# Apply "function" to each tuile of the taquin in the right order
-	def map(self, function, begin=0):
+	def map(self,  function, board=None, begin=0):
 		directions = [
 			{'begin': 0, 'end': self.size -1, 'step': 1},	# right
 			{'begin': 1, 'end': self.size -1, 'step': 1},	# down
 			{'begin': self.size -2, 'end': 0, 'step': -1},	# left
 			{'begin': self.size -2, 'end': 1, 'step': -1}	# up
 		]
+		board = self.board if board is None else board
 		data = {'x': 0, 'y': 0, 'i': 0}
 		ret = []
 		d = 0
 		while data['i'] <= self.size*self.size - 1:
 			if data['i'] >= begin:
-				r = function(data)
+				r = function(data, board)
 				if r is not None:
 					ret.append(r)
 			data['i'] += 1
@@ -52,65 +57,99 @@ class Taquin(object):
 		return ret
 
 	# check if the taquin is currently solved or not
-	def isSolved_map(self, data):
-		if self.board[data['x']][data['y']] == data['i'] + 1 or (data['i'] == self.size*self.size-1 and self.board[data['x']][data['y']] == 0):
+	def isSolved_map(self, data, board=None):
+		board = self.board if board is None else board
+		if board[data['x']][data['y']] == data['i'] + 1 or (data['i'] == self.size*self.size-1 and board[data['x']][data['y']] == 0):
 			return True
 		return False
 
-	def isSolved(self):
-		return all(self.map(self.isSolved_map))
+	def isSolved(self, board=None):
+		board = self.board if board is None else board
+		return all(self.map(self.isSolved_map, board))
 
-	def isSolvable__map(self, data):
-		return 1 if self.toCmp > self.board[data['x']][data['y']] and self.board[data['x']][data['y']] != 0 else 0
+	def inversionCount__map(self, data, board=None):
+		board = self.board if board is None else board
+		return 1 if self.toCmp > board[data['x']][data['y']] and board[data['x']][data['y']] != 0 else 0
 
-	def isSolvable_map(self, data):
-		self.toCmp = self.board[data['x']][data['y']]
-		return sum(self.map(self.isSolvable__map, data['i']))
+	def inversionCount_map(self, data, board=None):
+		self.toCmp = board[data['x']][data['y']]
+		board = self.board if board is None else board
+		return sum(self.map(self.inversionCount__map, board, data['i']))
 
-	def inversionCount(self):
-		return sum(self.map(self.isSolvable_map))
+	def inversionCount(self, board=None):
+		board = self.board if board is None else board
+		return sum(self.map(self.inversionCount_map, board))
 
-	def isSolvable(self):
-		return True if not self.inversionCount() % 2 else False
+	def isSolvable(self, board=None):
+		board = self.board if board is None else board
+		return True if not self.inversionCount(board) % 2 else False
 
-	def getCoor(self, board, value):
+	def getCoor(self, value, board=None):
 		return {
 			'x': [board.index(line) for line in board if value in line][0],
 			'y': [line.index(value) for line in board if value in line][0]
 		}
 
-	def getDestCoor_map(self, data):
+	def getDestCoor_map(self, data, board=None):
+		board = self.board if board is None else board
 		if data['i'] + 1 == self.toCmp or (self.toCmp == 0 and data['i'] == self.size*self.size-1):
 			return {'x': data['x'], 'y': data['y']}
 
-	def getDestCoor(self, value):
+	def getDestCoor(self, value, board=None):
+		board = self.board if board is None else board
 		self.toCmp = value;
-		ret = self.map(self.getDestCoor_map)
+		ret = self.map(self.getDestCoor_map, board)
 		return ret[0] if len(ret) == 1 else None;
 
 
-	def hash(self, board):
+	def hash(self, board=None):
+		board = self.board if board is None else board
 		tmp = []
 		for line in board:
 			tmp.append(hash(tuple(line)));
 		return hash(tuple(tmp))
 
-	def manhatan(self, board):
+	def manhatan(self, board=None):
+		board = self.board if board is None else board
 		score = 0
 		for line in board:
 			for tuile in line:
-				p =  self.getCoor(board, tuile);
+				p =  self.getCoor(tuile, board);
 				d = self.getDestCoor(tuile)
 				score += abs(p['x'] - d['x']) + abs(p['y'] - d['y'])
 		return score
 
-	def score(self, board):
-		#return self.inversionCount() / 2
+	def euclide(self, board=None):
+		board = self.board if board is None else board
+		score = 0
+		for line in board:
+			for tuile in line:
+				p =  self.getCoor(tuile, board);
+				d = self.getDestCoor(tuile)
+				d1 = abs(p['x'] - d['x'])
+				d2 = abs(p['y'] - d['y'])
+				score +=  sqrt(d1*d1+d2*d2)
+		return score
+
+	def all(self, board=None):
+		board = self.board if board is None else board
+		return self.manhatan(board) + self.euclide(board) / 5 + self.inversionCount(board) / 2
+
+
+
+	def score(self, board=None):
+		board = self.board if board is None else board
+		if self.heuristic == 'all':
+			return self.all(board)
+		if self.heuristic == 'inversions':
+			return self.inversionCount(board) / 2
+		if self.heuristic == 'euclide':
+			return self.euclide(board)
 		return self.manhatan(board)
 
-
-	def getLegalMove(self):
-		emptyCaseCoor = self.getCoor(self.board, 0);
+	def getLegalMove(self, board=None):
+		board = self.board if board is None else board
+		emptyCaseCoor = self.getCoor(0, board);
 		ret = []
 		if emptyCaseCoor['x'] > 0:
 			ret.append('down')
@@ -132,46 +171,66 @@ class Taquin(object):
 		if direction == 'right':
 			return 'left';
 
-	def move(self, board, direction):
-		p =  self.getCoor(board, 0);
-		if direction == "right":
-			board[p['x']][p['y']] = board[p['x']][p['y'] - 1];
-			board[p['x']][p['y'] - 1] = 0;
-		elif direction == "left":
-			board[p['x']][p['y']] = board[p['x']][p['y'] + 1];
-			board[p['x']][p['y'] + 1] = 0;
-		elif direction == "down":
-			board[p['x']][p['y']] = board[p['x'] - 1][p['y']];
-			board[p['x'] - 1][p['y']] = 0;
-		elif direction == "up":
-			board[p['x']][p['y']] = board[p['x'] + 1][p['y']];
-			board[p['x'] + 1][p['y']] = 0;
+	def move(self, direction, board=None):
+		board = self.board if board is None else board
+		if direction in self.getLegalMove(board):
+			p =  self.getCoor(0, board);
+			if direction == "right":
+				board[p['x']][p['y']] = board[p['x']][p['y'] - 1];
+				board[p['x']][p['y'] - 1] = 0;
+			elif direction == "left":
+				board[p['x']][p['y']] = board[p['x']][p['y'] + 1];
+				board[p['x']][p['y'] + 1] = 0;
+			elif direction == "down":
+				board[p['x']][p['y']] = board[p['x'] - 1][p['y']];
+				board[p['x'] - 1][p['y']] = 0;
+			elif direction == "up":
+				board[p['x']][p['y']] = board[p['x'] + 1][p['y']];
+				board[p['x'] + 1][p['y']] = 0;
 
-	def test_move(self, direction, test):
-		board = deepcopy(self.board)
-		self.move(board, direction)
-		return -2 if self.hash(board) in self.closed_list else self.score(board);
+	def test_move(self, direction, board=None):
+		board = self.board if board is None else board
+		self.move(direction, board)
+		score = -1 if self.hash(board) in self.closed_list else self.score(board);
+		self.move(self.backMove(direction), board)
+		return score
+
+	def getMoves(self, board=None):
+		board = self.board if board is None else board
+		moves = []
+		scores = []
+		for move in self.getLegalMove(board):
+			score = self.test_move(move, board)
+			if score > 0:
+				scores.append(score)
+				moves.append(move)
+		return moves, scores
 
 	def resolv(self):
 		while not self.isSolved():
-			scores = []
-			moves = self.getLegalMove();
-			for move in moves:
-				scores.append(self.test_move(move, True))
+			moves, scores = self.getMoves();		
 			if any([True if score >= 0 else False for score in scores]):
+				deep += 1
+				move = moves[scores.index(min([score for score in scores if score >= 0]))]
+				solution.append(move)
+				self.move(move);
+				self.closed_list.append(self.hash())     
 				self.timeComplexity += 1
 				self.spaceComplexity += 1
-				move = moves[scores.index(min([score for score in scores if score >= 0]))]
-				self.solution.append(move)
-				self.move(self.board, move);
-				self.closed_list.append(self.hash(self.board))
-			elif len(self.solution) >= 1:
+			elif len(solution) >= 1:
+				deep -= 1
+				self.move(self.backMove(solution[-1]));
+				solution.pop()
 				self.timeComplexity += 1
-				self.move(self.board, self.backMove(self.solution[-1]));
-				self.solution.pop()
-		return self.show()
+		return self.showSolution()
+
+	
+	def board(self, board=None):
+		board = self.board if board is None else board
+		for line in board:
+			print(line)
 			
-	def show(self):
+	def showSolution(self):
 		print('Time complexity :', self.timeComplexity)
 		print('Space complexity : ', self.spaceComplexity)
 		print('Move count  : ', len(self.solution))
